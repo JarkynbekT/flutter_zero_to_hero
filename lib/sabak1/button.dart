c5c4372c0f9e5e492c15769c960e3239b19903e9
JarkynbekT <carkinbek@gmail.com>
2024-06-21 13:35:55 +0300

    added button page

---
+++ lib/sabak1/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  const MyButton({super.key});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: Text("beni tikla"),
+    );
+  }
+}
